8df8859c0f355c57c1cc285f49598f34a758f0d4
Daniel Hjartland <squashical@gmail.com>
2022-11-01 09:28:34 +0100

    Add check if local storage function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import { Project } from './project';
 import { Todo } from './todo';
+import Storage from './storage';
+
+if (Storage.storageAvailable('localStorage')) {
+  console.log('localStorage is available');
+}
+else {
+  console.log('localStorage is not available');
+}
