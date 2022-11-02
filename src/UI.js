cdc91316f9b4776d56a1bd78b31000903f5c5586
Daniel Hjartland <squashical@gmail.com>
2022-11-02 10:03:31 +0100

    Add create project functionality

--- src/UI.js
+++ src/UI.js
@@ -1,6 +1,5 @@
 import Storage from './storage';
 import Project from './project';
-import Todo from './todo';
 
 export default class UI {
 
@@ -21,13 +20,14 @@ export default class UI {
           UI.createProject(project.name)
         }
       }) 
+    UI.initAddProjectBtns()
   }
 
   static loadTodos(projectName) {
-    const todoList = Storage.getTodoList()
-    const project = todoList.getProject(projectName)
-    const todos = project.getTodos()
-    todos.forEach((todo) => UI.createTodo(todo.getName(), todo.getDueDate()))
+    Storage.getTodoList()
+      .getProject(projectName)
+      .getTodos()
+      .forEach((todo) => UI.createTodo(todo.name, todo.dueDate))
   }
 
   static loadProjectContent(projectName) {
@@ -36,21 +36,64 @@ export default class UI {
       <h1 class="project-name">${projectName}</h1>
       <div class="todo-list" id=todo-list></div>`;
     if (projectName !== 'Due') {
-      projectView.innerHTML += ``
+      projectView.innerHTML += `
+        <button class="add-task-button button-todo" id="button-add-task">
+        </button>`
     }
 
     UI.loadTodos(projectName);
   }
 
+  static initAddProjectBtns() {
+    const addProjectBtn = document.getElementById('add-project-button');
+    const createProjectBtn = document.getElementById('create-project-button');
+    const cancelProjectBtn = document.getElementById('cancel-project-button');
+
+    addProjectBtn.addEventListener('click', UI.openCreateProjectForm)
+    cancelProjectBtn.addEventListener('click', UI.closeCreateProjectForm)
+    createProjectBtn.addEventListener('click', UI.addProject)
+
+  }
+
+  static openCreateProjectForm() {
+    const createProjectForm = document.getElementById('create-project-form');
+    createProjectForm.classList.toggle('hidden');
+  }
+
+  static closeCreateProjectForm() {
+    const createProjectForm = document.getElementById('create-project-form');
+    createProjectForm.classList.add('hidden');
+  }
+
+  static addProject() {
+    const projectNameInput = document.getElementById('project-form-input-name');
+    const projectName = projectNameInput.value;
+    
+    if (projectName === '') {
+      alert('Please enter a project name');
+      return;
+    }
+    else if (Storage.getTodoList().getProject(projectName)) {
+      alert('Project already exists');
+      return;
+    }
+
+    Storage.addProject(new Project(projectName));
+    UI.createProject(projectName);
+    UI.closeCreateProjectForm()
+    alert('Project added');
+  }
+
+
   static initProjectButtons() {
     const inboxBtn = document.getElementById('inbox-btn');
     const dueBtn = document.getElementById('due-btn');
-    const customProjectsBtn = document.querySelectorAll('.custom-projects-btn');
+    const customProjectsBtns = document.querySelectorAll('.custom-projects-button');
 
     inboxBtn.addEventListener('click', UI.openInbox);
     dueBtn.addEventListener('click', UI.openDue);
-    customProjectsBtn.forEach((btn) => {
-      btn.addEventListener('click', handleProjectClick);
+    customProjectsBtns.forEach((btn) => {
+      btn.addEventListener('click', UI.handleProjectClick);
     });
   }
 
@@ -76,12 +119,14 @@ export default class UI {
   }
 
   static handleProjectClick(e) {
-    const projectName = this.childdren[0].children[1].textContent;
+    console.log(e.target)
+    const projectName = this.children[0].children[1].textContent;
+    console.log(projectName)
     if (e.target.classList.contains('delete-item')) {
       UI.deleteProject(projectName, this);
       return;
     }
-    UI.openProject(projectName);
+    UI.openProject(projectName, this);
   }
 
   static createProject(projectName) {
@@ -96,10 +141,12 @@ export default class UI {
           <svg class="delete-item" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.3394 9.32245C16.7434 8.94589 16.7657 8.31312 16.3891 7.90911C16.0126 7.50509 15.3798 7.48283 14.9758 7.85938L12.0497 10.5866L9.32245 7.66048C8.94589 7.25647 8.31312 7.23421 7.90911 7.61076C7.50509 7.98731 7.48283 8.62008 7.85938 9.0241L10.5866 11.9502L7.66048 14.6775C7.25647 15.054 7.23421 15.6868 7.61076 16.0908C7.98731 16.4948 8.62008 16.5171 9.0241 16.1405L11.9502 13.4133L14.6775 16.3394C15.054 16.7434 15.6868 16.7657 16.0908 16.3891C16.4948 16.0126 16.5171 15.3798 16.1405 14.9758L13.4133 12.0497L16.3394 9.32245Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" fill="currentColor" /></svg>
         </div>
       </button>`;
+
+    UI.initProjectButtons();
   }
 
   static deleteProject(projectName, deleteButton) {
-    if (deleteButton.classList.contains('active')) clearProjectView();
+    if (deleteButton.classList.contains('active')) UI.clearProjectView();
     Storage.deleteProject(projectName);
     UI.clearProjects()
     UI.loadProjects();
@@ -121,7 +168,7 @@ export default class UI {
       <button class="button-todo">
         <div class="button-left">
           <div class="todo-icon">✓</div>
-          <div class="todo-text">Test</div>
+          <div class="todo-text">${taskName}</div>
         </div>
         <div class="button-right">${dueDate}</div>
       </button>`
