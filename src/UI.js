8eb16d64711e4ec5b1a0878a1482974ff5f61370
Daniel Hjartland <squashical@gmail.com>
2022-11-03 07:49:34 +0100

    Change formatting of todo items Fixed todo-icon class not being added on todo generation. Changed 'add new task' button text to 'add new todo' content to reflect app naming convention. Made add todo button darker to contrast todo items. Changed taskName to todoName as well.

--- src/UI.js
+++ src/UI.js
@@ -46,7 +46,7 @@ export default class UI {
               <div class="todo-icon">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="transparent" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M13 7C13 6.44772 12.5523 6 12 6C11.4477 6 11 6.44772 11 7V11H7C6.44772 11 6 11.4477 6 12C6 12.5523 6.44772 13 7 13H11V17C11 17.5523 11.4477 18 12 18C12.5523 18 13 17.5523 13 17V13H17C17.5523 13 18 12.5523 18 12C18 11.4477 17.5523 11 17 11H13V7Z" fill="currentColor" /></svg>
               </div>
-              <div class="todo-text">Add New Task</div>
+              <div class="todo-text">Add New Todo</div>
             </div>
             <div class="button-right"></div>
           </button>
@@ -261,7 +261,7 @@ export default class UI {
     projectsList.innerHTML = '';
   }
 
-  static createTodo(taskName, dueDate, completed) {
+  static createTodo(todoName, dueDate, completed) {
     const todoList = document.getElementById('todo-list');
     let markCompleteString;
     let classListAddString = '';
@@ -278,10 +278,10 @@ export default class UI {
     todoList.innerHTML += `
       <button class="button-todo${classListAddString}">
         <div class="button-left">
-          <div class="toggle-complete-icon">
+          <div class="toggle-complete-icon todo-icon">
             ${markCompleteString}
           </div>
-          <div class="todo-text">${taskName}</div>
+          <div class="todo-text">${todoName}</div>
         </div>
         <div class="button-right">
           <div class="due-text">${dueDate}</div>
